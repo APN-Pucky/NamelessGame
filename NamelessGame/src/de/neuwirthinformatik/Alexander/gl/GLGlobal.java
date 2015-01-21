bfee0f930a7c96adc81881892f79fe60039d328e
gw08neuwial <gw08neuwial@ID00117075.schulen.stadt-muenster.de>
2015-01-21 15:07:09 +0100

    VSYNC Bugs

--- NamelessGame/src/de/neuwirthinformatik/Alexander/gl/GLGlobal.java
+++ NamelessGame/src/de/neuwirthinformatik/Alexander/gl/GLGlobal.java
@@ -72,7 +72,7 @@ public class GLGlobal
 		rt.start();
 		//wait for window creation
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
